3306d224630a081cfb8115d18b704d66fbe21701
Aditya Gupta <mailadityaguptaa@gmail.com>
2024-06-26 17:57:36 +0530

    update

--- .deploy.prod.sh
+++ .deploy.prod.sh
@@ -8,29 +8,29 @@ git pull origin master
 
 if [ ! -f /var/www/first_run ]; then
     echo "Running composer install..."
-    composer install --optimize-autoloader
-    php artisan migrate:fresh
+    docker compose exec -it app composer install --optimize-autoloader
+    docker compose exec -it app php artisan migrate:fresh
 
     echo "Running npm install..."
-    /usr/bin/node /usr/bin/npm install
+    docker compose exec -it app /usr/bin/node /usr/bin/npm install
 
     echo "Running php artisan key:generate"
-    php artisan key:generate
+    docker compose exec -it app php artisan key:generate
 
     echo "Creating first_run file..."
-    touch /var/www/first_run
+    docker compose exec -it app touch /var/www/first_run
 
 else
     echo "first_run file already exists. Skipping initial setup..."
 
 
-    composer install --no-dev --optimize-autoloader --no-interaction --prefer-dist
-    php artisan migrate --force
-    php artisan storage:link
-    php artisan optimize:clear
-    php artisan optimize
+    docker compose exec -it app composer install --no-dev --optimize-autoloader --no-interaction --prefer-dist
+    docker compose exec -it app php artisan migrate --force
+    docker compose exec -it app php artisan storage:link
+    docker compose exec -it app php artisan optimize:clear
+    docker compose exec -it app php artisan optimize
 
-    /usr/bin/node /usr/bin/npm install
-    /usr/bin/node /usr/bin/npm run build
+    docker compose exec -it app /usr/bin/node /usr/bin/npm install
+    docker compose exec -it app /usr/bin/node /usr/bin/npm run build
 
 fi
